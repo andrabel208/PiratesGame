f02d11094df99d12ccb59f143cda69e2be051917
Andra-Evelina Beldiman <152090998+andrabel208@users.noreply.github.com>
2026-01-27 00:05:40 +0200

    Update MyWorld.java with new content

--- PiratesGame/MyWorld.java
+++ PiratesGame/MyWorld.java
@@ -1,13 +1,17 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
 
 /**
- * Write a description of class MyWorld here.
+ * Main world for the Pirates Game.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
 public class MyWorld extends World
 {
+    private Pirate pirate;
+    private int score;
+    private int coins;
 
     /**
      * Constructor for objects of class MyWorld.
@@ -17,7 +21,32 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        // Show a message in the center of the world
-        showText("hello anna!! kisses", 300, 200);
+        pirate = new Pirate();
+        addObject(pirate, 300, 200);
+        showText("Welcome to Art Island!", 300, 30);
+        runArtIsland();
+    }
+
+    private void runArtIsland()
+    {
+        QuestionBank bank = new QuestionBank();
+        List<Question> questions = bank.getArtQuestions();
+        for (Question question : questions)
+        {
+            String answer = Greenfoot.ask(question.getPrompt());
+            if (question.isCorrect(answer))
+            {
+                score += 10;
+                coins += 5;
+                showText("Correct!", 300, 200);
+            }
+            else
+            {
+                showText("Wrong! Correct: " + question.getPrimaryAnswer(), 300, 200);
+            }
+            Greenfoot.delay(50);
+            showText("", 300, 200);
+        }
+        showText("Art Island complete! Score: " + score + " Coins: " + coins, 300, 200);
     }
 }
